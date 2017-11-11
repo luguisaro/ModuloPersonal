f800e15cb3304e7e4e3f6e5bc55372b5a258aeec
luguisaro <luguisaro@hotmail.es>
2017-11-11 14:08:29 -0600

    segunda version tipo moneda

---
+++ TipoMonedaV2(1)/tipoMoneda/tipoMoneda/Form3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tipoMoneda
+{
+    public partial class Form3 : Form
+    {
+        string Num_form = "5303";
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
